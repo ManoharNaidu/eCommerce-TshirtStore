801e8785b5376bb41e77ce295c3f8a8919566ab8
ManoharNaidu <beesettim27@gmail.com>
2024-03-24 15:03:47 +0530

    Added admin routes for orders - get, update, delete

--- routes/order.js
+++ routes/order.js
@@ -2,11 +2,17 @@ const express = require('express');
 const router = express.Router();
 const { isLoggedIn, customRole } = require('../middlewares/user');
 
-const {createOrder, getOrder, getMyOrders} = require('../controllers/orderController');
+const {createOrder, getOrder, getMyOrders, adminGetAllOrders, adminGetOneOrder, adminUpdateOrder, adminDeleteOneOrder} = require('../controllers/orderController');
 
 router.route("/order/create").post(isLoggedIn, createOrder)
 router.route("/order/:id").get(isLoggedIn, getOrder)
 router.route("/myOrders").get(isLoggedIn, getMyOrders)
+
+// admin routes
+router.route("/admin/orders").get(isLoggedIn, customRole("admin"), adminGetAllOrders)
+router.route("/admin/order/:id").get(isLoggedIn, customRole("admin"), adminGetOneOrder)
+router.route("/admin/order/:id").put(isLoggedIn, customRole("admin"), adminUpdateOrder)
+router.route("/admin/order/:id").delete(isLoggedIn, customRole("admin"), adminDeleteOneOrder)
 
 
 module.exports = router;
